8ff1aa1d1e12a232fc64d0f17a554ce38c1f0822
inigo.cobian <inigo.cobian@gmail.com>
2026-03-29 23:01:00 +0200

    fix: correct include paths

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <fstream>
-#include <io/http_client.hpp>
-#include <io/temp_file.hpp>
 
 #include "core/logger.hpp"
 #include "gtfs/gtfs_manager.hpp"
+#include "io/http_client.hpp"
+#include "io/temp_file.hpp"
 
 int main() {
     core::Logger::init();
