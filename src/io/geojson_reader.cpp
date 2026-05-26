945c070a5bd0fec7a7430b519877e2da1494dad7
inigo.cobian <inigo.cobian@gmail.com>
2026-05-26 03:27:26 +0200

    feat: validate if json is geojson

--- src/io/geojson_reader.cpp
+++ src/io/geojson_reader.cpp
@@ -3,16 +3,30 @@
 #include <fstream>
 
 namespace io {
+    constexpr std::array validGeojsonTypes{
+        "Point", "MultiPoint", "LineString", "MultiLineString",
+        "Polygon", "MultiPolygon", "GeometryCollection",
+        "Feature", "FeatureCollection"
+    };
+
     nlohmann::json GeoJsonReader::read(const std::string &file) {
+        std::ifstream f(file);
         try {
-            std::ifstream f(file);
-            nlohmann::json data = nlohmann::json::parse(f);
-            if (data.empty()) {
-                throw std::runtime_error("Cannot parse file or empty file");
-            }
-            return data;
+            f = std::ifstream(file);
         } catch (const std::exception &e) {
             throw std::runtime_error("Could not open file " + file + ". Reason: " + e.what());
         }
+
+        nlohmann::json data = nlohmann::json::parse(f);
+        if (data.empty()) {
+            throw std::runtime_error("Cannot parse file or empty file");
+        }
+        // The GeoJSON could be validated further, but for now, let's do a simple analysis
+        if (data.contains("type") && data.at("type").is_string()) {
+            if (auto type = data["type"].get<std::string_view>(); !std::ranges::contains(validGeojsonTypes, type)) {
+                throw std::runtime_error("Cannot validate type of geoJSON");
+            }
+        }
+        return data;
     }
 }
