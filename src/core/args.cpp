97c9fbc1d4fb987bcab967b686062745cf956893
inigo.cobian <inigo.cobian@gmail.com>
2026-06-12 01:46:35 +0200

    feat: update args for mongodb

--- src/core/args.cpp
+++ src/core/args.cpp
@@ -5,36 +5,44 @@
 namespace core {
     using commandtype = std::function<void(const std::string &, std::vector<std::string>::const_iterator, std::vector<std::string>::const_iterator)>;
 
-    int Args::init(int argc, char *argv[]) {
-        args::ArgumentParser parser("Options for Garraiobide.", "Kontuz nasa eta trenaren arteko tartearekin.");
-        //args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-        //args::CompletionFlag completion(parser, {"complete"});
+    args::ArgumentParser parser("Options for Garraiobide.", "Kontuz nasa eta trenaren arteko tartearekin.");
+    //args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+    //args::CompletionFlag completion(parser, {"complete"});
 
-        args::Group commands(parser, "commands");
-        args::Command add(commands, "add", "add a single resource");
-        args::Command run(commands, "run", "runs the main program");
-        args::Group arguments(parser, "arguments", args::Group::Validators::DontCare, args::Options::Global);
-        args::ValueFlag<std::string> resourceType(arguments, "type", "resource type. Allowed: gtfs", {"type"});
-        args::ValueFlag<std::string> resourcePath(arguments, "path", "path or URL of the resource", {"url"});
-        args::ValueFlag<std::string> resourceName(arguments, "name", "name of the resource", {"name"});
-        args::HelpFlag h(arguments, "help", "help", {'h', "help"});
-        //args::PositionalList<std::string> pathsList(arguments, "paths", "files to import");
+    args::Group commands(parser, "commands");
+    args::Command add(commands, "add", "add a single resource");
+    args::Command run(commands, "run", "runs the main program");
+
+    args::Group arguments(parser, "arguments", args::Group::Validators::DontCare, args::Options::Global);
+    args::ValueFlag<std::string> resourceType(arguments, "type", "resource type. Allowed: gtfs", {"type"});
+    args::ValueFlag<std::string> resourcePath(arguments, "path", "path or URL of the resource", {"url"});
+    args::ValueFlag<std::string> resourceName(arguments, "name", "name of the resource", {"name"});
+    args::HelpFlag h(arguments, "help", "help", {'h', "help"});
+    //args::PositionalList<std::string> pathsList(arguments, "paths", "files to import");
+
+    args::Group mongodb(parser, "MongoDB arguments", args::Group::Validators::All, args::Options::Global);
+    args::ValueFlag<std::string> mongoUser(mongodb, "user", "MongoDB username", {"mongoUser"});
+    args::ValueFlag<std::string> mongoPass(mongodb, "password", "MongoDB password", {"mongoPass"});
+    args::ValueFlag<std::string> mongoUrl(mongodb, "url", "MongoDB URL", {"mongoUrl"});
+
+    int Args::init(int argc, char *argv[]) {
 
         try {
             parser.ParseCLI(argc, argv);
         } catch (const args::Completion &e) {
             std::cout << e.what();
-            return 0;
+            return 1;
         }
         catch (const args::Help &) {
             std::cout << parser;
-            return 0;
         }
         catch (const args::ParseError &e) {
             std::cerr << e.what() << std::endl;
             std::cerr << parser;
             return 1;
         }
+        mongdbUri = "mongodb://" + mongoUser.Get() + ":" + mongoPass.Get() + "@" + mongoUrl.Get();
+
         return 0;
     }
 }
--- src/core/args.hpp
+++ src/core/args.hpp
@@ -1,8 +1,10 @@
 #pragma once
+#include <string>
 
 namespace core {
     class Args {
     public:
         static int init(int argc, char *argv[]);
+        inline static std::string mongdbUri;
     };
 }
