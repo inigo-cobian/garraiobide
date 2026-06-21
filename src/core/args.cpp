3698738a9eba851a40edb03a554f8386cd00da48
inigo.cobian <inigo.cobian@gmail.com>
2026-06-21 16:43:52 +0200

    feat: keep structuring arg parser

--- src/core/args.cpp
+++ src/core/args.cpp
@@ -3,110 +3,103 @@
 #include <args.hxx>
 
 namespace core {
-    void runStartup(const std::string &mongoUser, const std::string &mongoPass,
-                    const std::string &mongoUrl, const std::string &pgUser,
-                    const std::string &pgPass, const std::string &pgUrl,
-                    const std::string &logLevel) {
-        std::cout << "Running app with log level: " << logLevel << std::endl;
-        std::cout << "MongoDB: " << mongoUser << "@" << mongoUrl << std::endl;
-        std::cout << "PostGIS: " << pgUser << "@" << pgUrl << std::endl;
+    void runStartup(const RunConfig& run_config) {
+        std::cout << "Running app with log level: " << run_config.logLevel << std::endl;
+        std::cout << "MongoDB: " << run_config.mongoUser << "@" << run_config.mongoUrl << std::endl;
+        std::cout << "PostGIS: " << run_config.pgUser << "@" << run_config.pgUrl << std::endl;
     }
 
-    void statsStartup(const std::string &logLevel) {
-        std::cout << "Displaying statistics with log level: " << logLevel << std::endl;
+    void statsStartup(const StartupConfig& startup_config) {
+        std::cout << "Displaying statistics with log level: " << startup_config.logLevel << std::endl;
     }
 
-    void ingestStartup(const std::string &name, const std::string &type,
-                       const std::string &url, const std::string &creds,
-                       const std::string &logLevel) {
-        std::cout << "Ingesting " << type << " resource: " << name << std::endl;
-        std::cout << "URL: " << url << ", Credentials: " << creds << std::endl;
+    void ingestStartup(const IngestConfig& ingest_config) {
+        std::cout << "Ingesting " << ingest_config.type << " resource: " << ingest_config.name << std::endl;
+        std::cout << "URL: " << ingest_config.url << ", Credentials: " << ingest_config.credentials << std::endl;
     }
 
-    using commandtype = std::function<void(const std::string &, std::vector<std::string>::const_iterator,
-                                           std::vector<std::string>::const_iterator)>;
+    std::expected<StartupConfig, std::runtime_error> Args::parse_args(int argc, char *argv[]) {
+        args::ArgumentParser parser("Options for Garraiobide.", "Kontuz nasa eta trenaren arteko tartearekin.");
+        args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+        args::ValueFlag<std::string> logLevel(parser, "level", "Set the logging level (error, warning, info, debug)",
+                                              {'l', "log"}, "info");
 
-    args::ArgumentParser parser("Options for Garraiobide.", "Kontuz nasa eta trenaren arteko tartearekin.");
-    args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-    args::ValueFlag<std::string> logLevel(parser, "level", "Set the logging level (error, warning, info, debug)",
-                                          {'l', "log"}, "info");
+        args::Group commands(parser, "commands", args::Group::Validators::AtMostOne);
 
-    args::Group commands(parser, "commands", args::Group::Validators::AllOrNone);
+        args::Command runCmd(
+            commands,
+            "run",
+            "Run the main application",
+            [&](args::Subparser &s) {
+                args::ValueFlag<std::string> mongoUser(s, "user", "MongoDB username", {"mongo-user"});
+                args::ValueFlag<std::string> mongoPass(s, "password", "MongoDB password", {"mongo-pass"});
+                args::ValueFlag<std::string> mongoUrl(s, "url", "MongoDB URL", {"mongo-url"});
+                args::ValueFlag<std::string> pgUser(s, "user", "PostGIS username", {"pg-user"});
+                args::ValueFlag<std::string> pgPass(s, "password", "PostGIS password", {"pg-pass"});
+                args::ValueFlag<std::string> pgUrl(s, "url", "PostGIS URL", {"pg-url"});
 
-    args::Command runCmd(
-        commands,
-        "run",
-        "Run the main application",
-        [](args::Subparser &s) {
-            args::ValueFlag<std::string> mongoUser(s, "user", "MongoDB username", {"mongo-user"});
-            args::ValueFlag<std::string> mongoPass(s, "password", "MongoDB password", {"mongo-pass"});
-            args::ValueFlag<std::string> mongoUrl(s, "url", "MongoDB URL", {"mongo-url"});
-            args::ValueFlag<std::string> pgUser(s, "user", "PostGIS username", {"pg-user"});
-            args::ValueFlag<std::string> pgPass(s, "password", "PostGIS password", {"pg-pass"});
-            args::ValueFlag<std::string> pgUrl(s, "url", "PostGIS URL", {"pg-url"});
+                s.Parse();
 
-            s.Parse();
+                if (!mongoUser || !mongoPass || !mongoUrl || !pgUser || !pgPass || !pgUrl) {
+                    std::cerr << "Error: Missing required arguments for 'run'." << std::endl;
+                    return;
+                }
 
-            if (!mongoUser || !mongoPass || !mongoUrl || !pgUser || !pgPass || !pgUrl) {
-                std::cerr << "Error: Missing required arguments for 'run'." << std::endl;
-                return;
+                std::string level = args::get(logLevel);
             }
+        );
 
-            std::string level = args::get(logLevel);
-            runStartup(args::get(mongoUser), args::get(mongoPass), args::get(mongoUrl),
-                       args::get(pgUser), args::get(pgPass), args::get(pgUrl), level);
-        }
-    );
-
-    args::Command statsCmd(
-        commands,
-        "stats",
-        "Display statistics",
-        [](args::Subparser &s) {
-            s.Parse(); // No extra arguments needed
-            statsStartup(args::get(logLevel));
-        }
-    );
-
-
-    args::Command ingestCmd(
-        commands,
-        "ingest",
-        "Ingest a resource",
-        [](args::Subparser &s) {
-            args::ValueFlag<std::string> resourceName(s, "name", "Resource name", {"name"});
-            args::ValueFlag<std::string> resourceType(s, "type", "Resource type", {"type"});
-            args::ValueFlag<std::string> resourceUrl(s, "url", "Resource URL", {"url"});
-            args::ValueFlag<std::string> resourceCreds(s, "credentials", "Resource credentials", {"creds"});
-
-            s.Parse();
-
-            if (!resourceName || !resourceType || !resourceUrl || !resourceCreds) {
-                std::cerr << "Error: Missing required arguments for 'ingest'." << std::endl;
-                return;
+        args::Command statsCmd(
+            commands,
+            "stats",
+            "Display statistics",
+            [&](args::Subparser &s) {
+                s.Parse();
             }
+        );
 
-            ingestStartup(args::get(resourceName), args::get(resourceType),
-                          args::get(resourceUrl), args::get(resourceCreds),
-                          args::get(logLevel));
-        }
-    );
 
-    int Args::parse_args(int argc, char *argv[]) {
+        args::Command ingestCmd(
+            commands,
+            "ingest",
+            "Ingest a resource",
+            [&](args::Subparser &s) {
+                args::ValueFlag<std::string> resourceName(s, "name", "Resource name", {"name"});
+                args::ValueFlag<std::string> resourceType(s, "type", "Resource type", {"type"});
+                args::ValueFlag<std::string> resourceUrl(s, "url", "Resource URL", {"url"});
+                args::ValueFlag<std::string> resourceCreds(s, "credentials", "Resource credentials", {"creds"});
+
+                s.Parse();
+
+                if (!resourceName || !resourceType || !resourceUrl || !resourceCreds) {
+                    std::cerr << "Error: Missing required arguments for 'ingest'." << std::endl;
+                }
+            }
+        );
         try {
             parser.ParseCLI(argc, argv);
         } catch (const args::Completion &e) {
-            std::cout << e.what();
-            return 1;
+            throw std::runtime_error(e.what());
         } catch (const args::Help &) {
             std::cout << parser;
         } catch (const args::ParseError &e) {
-            std::cerr << e.what() << std::endl;
-            std::cerr << parser;
-            return 1;
+            throw std::runtime_error(e.what() + parser);
         }
         //auto mongoUrl = "mongodb://" + mongoUser.Get() + ":" + mongoPass.Get() + "@" + mongoUrl.Get() + "/?authSource=admin&authMechanism=SCRAM-SHA-256";
+        if (runCmd) {
+            RunConfig run_config{};
+            runStartup(run_config);
+        }
 
-        return 0;
+        if (statsCmd) {
+            StatsConfig stats_config{};
+            statsStartup(stats_config);
+        }
+
+        if (ingestCmd) {
+            IngestConfig ingest_config{};
+            ingestStartup(ingest_config);
+        }
+        throw std::runtime_error("No mode found");
     }
 }
--- src/core/args.hpp
+++ src/core/args.hpp
@@ -1,29 +1,40 @@
 #pragma once
+#include <expected>
+#include <stdexcept>
 #include <string>
 
 namespace core {
-    inline struct RunConfig {
+    enum LaunchMode {
+        Run, Ingest, Stats
+    };
+
+    struct StartupConfig {
+        LaunchMode mode;
+        std::string logLevel;
+    };
+
+    struct RunConfig : public StartupConfig {
         std::string mongoUser;
         std::string mongoPass;
         std::string mongoUrl;
         std::string pgUser;
         std::string pgPass;
         std::string pgUrl;
-    } run_config;
+    };
 
-    inline struct IngestConfig {
+    struct IngestConfig : public StartupConfig {
         std::string name;
         std::string type;
         std::string url;
         std::string credentials;
-    } ingest_config;
+    };
 
-    inline struct StatsConfig {
+    struct StatsConfig : public StartupConfig {
         // empty
-    } stats_config;
+    };
 
     class Args {
     public:
-        static int parse_args(int argc, char *argv[]);
+        [[nodiscard]] static std::expected<StartupConfig, std::runtime_error> parse_args(int argc, char *argv[]);
     };
 }
