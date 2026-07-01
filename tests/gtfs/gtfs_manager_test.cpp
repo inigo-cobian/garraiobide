ae42320d50f3f702848320b0d54d2045e624f6d2
inigo.cobian <inigo.cobian@gmail.com>
2026-07-01 11:38:19 +0200

    test: update gtfs_manager tests

--- tests/gtfs/gtfs_manager_test.cpp
+++ tests/gtfs/gtfs_manager_test.cpp
@@ -1,17 +1,16 @@
 #include <gtest/gtest.h>
 #include "gtfs/gtfs_manager.hpp"
 
-// source: https://github.com/google/transit/blob/master/gtfs/spec/en/examples/sample-feed-1.zip
-const std::string sampleFeed("files/sample-feed-1.zip");
+const std::string sampleFeed("https://www.bilbao.eus/opendata/datos/bilbobus-gtfs");
 
 TEST(GtfsManagerTest, loadFeedValid) {
     gtfs::GtfsManager manager;
 
-    EXPECT_NO_THROW(manager.load_feed(sampleFeed));
-    EXPECT_EQ(manager.get_stops().size(), 9);
+    EXPECT_NO_THROW(manager.load_feed("test", sampleFeed));
+    EXPECT_EQ(manager.get_stops().size(), 537);
     EXPECT_EQ(manager.get_agencies().size(), 1);
-    EXPECT_EQ(manager.get_routes().size(), 5);
-    EXPECT_TRUE(manager.get_shapes()->empty());
-    EXPECT_EQ(manager.get_trips().size(), 11);
-    EXPECT_EQ(manager.get_stop_times().size(), 28);
+    EXPECT_EQ(manager.get_routes().size(), 56);
+    EXPECT_FALSE(manager.get_shapes()->empty());
+    EXPECT_EQ(manager.get_trips().size(), 189);
+    EXPECT_EQ(manager.get_stop_times().size(), 558515);
 }
